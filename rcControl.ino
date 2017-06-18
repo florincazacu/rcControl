0ec7f22249df8d64e7196a93ad01d1e42c46cee6
Florin <florin.ics@gmail.com>
2017-06-18 21:27:26 +0300

    Commented code

--- rcControl.ino
+++ rcControl.ino
@@ -13,19 +13,19 @@ Servo myServo; // servo name
 #define BACK_RIGHT 74
 #define STOP 83
 
-int bluetoothTx = 4; // bluetooth tx to 10 pin
-int bluetoothRx = 2; // bluetooth rx to 11 pin
+int bluetoothTx = 4; // bluetooth tx to 4 pin
+int bluetoothRx = 2; // bluetooth rx to 2 pin
 
 // Motor1 output takes pins 6 and 9 instead of 3 and 5
 int motorA1  = 6;  // Pin  6 of L298
 int motorA2  = 9;  // Pin  9 of L298
-int servoPwr = 3;
-int servoGnd = 5;
+int servoPwr = 3;  // Pin  3 of L298
+int servoGnd = 5;  // Pin  5 of L298
 
-int state;
-int servoPosition = 90;
-int maxSpeed = 255;
-int vSpeed = maxSpeed;
+int stream; // Received data from bluetooth
+int servoPosition = 90; // Default servomotor position for straight car movement
+int maxSpeed = 255; // Maximum engine speed
+int currentSpeed = maxSpeed; // Current engine speed
 int delayTime = 100;
 int turnIncrement = 5;
 int maxLeftAngle = 10;
@@ -39,25 +39,25 @@ void setup() {
   pinMode(servoGnd, OUTPUT);
   pinMode(servoPwr, OUTPUT);
   analogWrite (servoGnd, 0);
-  myServo.attach(11); // attach servo signal wire to pin 9
+  myServo.attach(11); // Attach servo signal wire to pin 11
   myServo.write(servoPosition);
   //Setup usb serial connection to computer
   Serial.begin(115200);
-
   //Setup Bluetooth serial connection to android
   mySerial.begin(115200);
 }
 
+// Power servo
 void setServoMotorOutput(int voltage) {
   analogWrite(servoPwr, voltage);
 }
 
 void goForward() {
   Serial.println("Forward");
-  analogWrite(motorA1, vSpeed);
+  analogWrite(motorA1, currentSpeed);
   analogWrite(motorA2, 0);
-  Serial.print("Forward vSpeed: ");
-  Serial.println (vSpeed);
+  Serial.print("Forward currentSpeed: ");
+  Serial.println (currentSpeed);
   Serial.print("motorA1: ");
   Serial.print(analogRead(motorA1));
   Serial.print(" motorA2: ");
@@ -67,9 +67,9 @@ void goForward() {
 void goBack() {
   Serial.println("Back");
   analogWrite(motorA1, 0);
-  analogWrite(motorA2, vSpeed);
-  Serial.print("Back vSpeed: ");
-  Serial.println (vSpeed);
+  analogWrite(motorA2, currentSpeed);
+  Serial.print("Back currentSpeed: ");
+  Serial.println (currentSpeed);
   Serial.print("motorA1: ");
   Serial.print(analogRead(motorA1));
   Serial.print(" motorA2: ");
@@ -111,16 +111,17 @@ void stopCar() {
 
 void loop() {
 
-  //  //Read from bluetooth and write to usb serial
+//Read from bluetooth and write to usb serial
   if (mySerial.available()) {
-    state = mySerial.read();
-    Serial.print("state received: ");
-    Serial.println(state);
+    stream = mySerial.read();
+    Serial.print("stream received: ");
+    Serial.println(stream);
   }
 
-  switch (state) {
-      Serial.print("state: ");
-      Serial.println(state);
+// Manipulate received data
+  switch (stream) {
+      Serial.print("stream: ");
+      Serial.println(stream);
     case 'q':
     case STOP:
       setServoMotorOutput(0);
@@ -130,17 +131,17 @@ void loop() {
     case '1':
     case '2':
     case '3':
-      vSpeed = maxSpeed * 0.3;
+      currentSpeed = maxSpeed * 0.3;
       break;
     case '4':
     case '5':
     case '6':
-      vSpeed = maxSpeed * 0.5;
+      currentSpeed = maxSpeed * 0.5;
       break;
     case '7':
     case '8':
     case '9':
-      vSpeed = maxSpeed;
+      currentSpeed = maxSpeed;
       break;
     case FORWARD:
       goForward();
